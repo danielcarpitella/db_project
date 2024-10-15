71e4a217079e710130a0ec4a714671ef9f33b06d
danielcarpitella <danielcarpitella@gmail.com>
2024-10-15 18:01:22 +0200

    Build: added profile edit page for buyer

--- app/main/forms.py
+++ app/main/forms.py
@@ -19,5 +19,12 @@ class ProductForm(FlaskForm):
         self.category_id.choices = [(category.id, category.title) for category in Category.query.all()]
         
 
-
+class EditProfileForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=1, max=50)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=50)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=1, max=100)])
+    phone = StringField('Phone Number', validators=[DataRequired(), Length(min=1, max=20)])
+    shipping_address = StringField('Shipping Address', validators=[DataRequired(), Length(min=1, max=200)])
+    payment_method = SelectField('Payment Method', choices=[('Card', 'Card'), ('Paypal', 'Paypal'), ('Klarna', 'Klarna')], validators=[DataRequired()])
+    submit = SubmitField('Save changes')
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, abort, url_for, redirect, flash 
 from . import main
-from app.models import Product, Category, Review, Cart, ProductsCart, Seller, Order, ProductOrderQuantity, User, SellerOrder
+from app.models import Product, Category, Review, Cart, ProductsCart, Seller, Order, ProductOrderQuantity, User, SellerOrder, Buyer
 from flask_login import current_user, login_required
 from app import db
-from flask import session
+from .forms import EditProfileForm
 
 
 @main.route('/')
@@ -18,7 +18,29 @@ def user(username):
     return render_template('user.html', user=user)
 
 
-
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    buyer = Buyer.query.filter_by(user_id=current_user.id).first()
+    if not buyer:
+        abort(403)
+    
+    form = EditProfileForm(obj=current_user)
+    if form.validate_on_submit():
+        current_user.first_name = form.first_name.data
+        current_user.last_name = form.last_name.data
+        current_user.email = form.email.data
+        current_user.phone = form.phone.data
+        buyer.shipping_address = form.shipping_address.data
+        buyer.payment_method = form.payment_method.data
+        db.session.commit()
+        flash('Profile updated successfully.', 'success')
+        return redirect(url_for('main.user', username=current_user.id))
+    
+    form.shipping_address.data = buyer.shipping_address
+    form.payment_method.data = buyer.payment_method
+    
+    return render_template('edit_profile.html', form=form)
 
 
 
